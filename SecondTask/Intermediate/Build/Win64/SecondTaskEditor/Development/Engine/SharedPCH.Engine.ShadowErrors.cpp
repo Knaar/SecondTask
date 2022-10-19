f753c68077624caf4501b1fb6a13eff46800a975
Bordunos <Mr.bordunos@mail.ru>
2022-10-19 22:34:30 +0300

    FirstStep

---
+++ SecondTask/Intermediate/Build/Win64/SecondTaskEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30146
+#include "E:/UnrealProjects/SecondTask/SecondTask/Intermediate/Build/Win64/SecondTaskEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ SecondTask/Intermediate/Build/Win64/UE4Editor/Inc/SecondTask/SecondTaskGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SECONDTASK_SecondTaskGameModeBase_generated_h
+#error "SecondTaskGameModeBase.generated.h already included, missing '#pragma once' in SecondTaskGameModeBase.h"
+#endif
+#define SECONDTASK_SecondTaskGameModeBase_generated_h
+
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_SPARSE_DATA
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_RPC_WRAPPERS
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASecondTaskGameModeBase(); \
+	friend struct Z_Construct_UClass_ASecondTaskGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASecondTaskGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/SecondTask"), NO_API) \
+	DECLARE_SERIALIZER(ASecondTaskGameModeBase)
+
+
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASecondTaskGameModeBase(); \
+	friend struct Z_Construct_UClass_ASecondTaskGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ASecondTaskGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/SecondTask"), NO_API) \
+	DECLARE_SERIALIZER(ASecondTaskGameModeBase)
+
+
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASecondTaskGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASecondTaskGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASecondTaskGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASecondTaskGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASecondTaskGameModeBase(ASecondTaskGameModeBase&&); \
+	NO_API ASecondTaskGameModeBase(const ASecondTaskGameModeBase&); \
+public:
+
+
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASecondTaskGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASecondTaskGameModeBase(ASecondTaskGameModeBase&&); \
+	NO_API ASecondTaskGameModeBase(const ASecondTaskGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASecondTaskGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASecondTaskGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASecondTaskGameModeBase)
+
+
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_12_PROLOG
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_SPARSE_DATA \
+	SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_RPC_WRAPPERS \
+	SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_INCLASS \
+	SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_SPARSE_DATA \
+	SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	SecondTask_Source_SecondTask_SecondTaskGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SECONDTASK_API UClass* StaticClass<class ASecondTaskGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SecondTask_Source_SecondTask_SecondTaskGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
